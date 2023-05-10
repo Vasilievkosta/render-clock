1b01f7faed412352548beaa0860f045695fa9a0d
Vasilievkosta <vasilievkosta@gmail.com>
2023-05-10 19:39:01 +0300

    added key to app file

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ let token = () => {
 }
 
 const app = express();
-app.use(cookieParser())
+let secret = 'qwerty'
+app.use(cookieParser(secret))
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
