25262ca640fb102f08d206ef4913ee2531699197
Vasilievkosta <vasilievkosta@gmail.com>
2023-11-14 08:55:37 +0200

    modified db

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 	const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
-	const proConfig = `postgres://test_postgres_7wf0_user:xtAaYfGGEwFIxS2aOWvv7Icl38yUCd07@dpg-cjrllilhe99c73cft9t0-a/test_postgres_7wf0`
+	const proConfig = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
 
 const pool = new Pool({	
     connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig,
