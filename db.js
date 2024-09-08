73e10fe3ef6ffe6289aade1229b0a3a88061c309
Vasilievkosta <vasilievkosta@gmail.com>
2024-09-09 00:37:22 +0300

    refactor: change url proConfig to db

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 
 	const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
-	const proConfig = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+	// const proConfig = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+	const proConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
 const pool = new Pool({	
     connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig,
